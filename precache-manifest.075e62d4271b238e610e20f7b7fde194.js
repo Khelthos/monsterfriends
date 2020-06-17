0b37cfe0c6a792ea086b6682eeb89917fcb59b00
Khelthos <khelthos@hotmail.it>
2020-06-17 10:50:19 +0200

    Updates

---
+++ precache-manifest.075e62d4271b238e610e20f7b7fde194.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34556c051f7c412f8d32e61544c14d66",
+    "url": "/monsterfriends/index.html"
+  },
+  {
+    "revision": "bffc5a90c254397f004a",
+    "url": "/monsterfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fceedb20f1aab3eae1c8",
+    "url": "/monsterfriends/static/css/main.5980a21c.chunk.css"
+  },
+  {
+    "revision": "bffc5a90c254397f004a",
+    "url": "/monsterfriends/static/js/2.16781374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterfriends/static/js/2.16781374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fceedb20f1aab3eae1c8",
+    "url": "/monsterfriends/static/js/main.8498be70.chunk.js"
+  },
+  {
+    "revision": "a463026de579d4a89282",
+    "url": "/monsterfriends/static/js/runtime-main.e34999b9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/monsterfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
